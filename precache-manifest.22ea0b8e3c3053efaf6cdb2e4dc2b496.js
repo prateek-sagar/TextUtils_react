7ebe98bf4d830bc291be319956b725af292c9778
prateek-sagar <prateeksagar806@gmail.com>
2023-09-15 11:55:50 +0530

    Updates

---
+++ precache-manifest.22ea0b8e3c3053efaf6cdb2e4dc2b496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c14e78d1913823b8113",
+    "url": "/TextUtils_react/static/css/main.3b405d15.chunk.css"
+  },
+  {
+    "revision": "1c14e78d1913823b8113",
+    "url": "/TextUtils_react/static/js/main.1c14e78d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils_react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils_react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "e928f1cf903540eeb019",
+    "url": "/TextUtils_react/static/js/runtime~main.e928f1cf.js"
+  },
+  {
+    "revision": "d71bae9e7ceffedf406bc2cd7a756e96",
+    "url": "/TextUtils_react/index.html"
+  }
+];
